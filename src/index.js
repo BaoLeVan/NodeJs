df97072299274e9b1470a5e3e83ef2b991778b58
baole1102 <levannhatbao29@gmail.com>
2024-06-09 23:57:13 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,21 +4,11 @@ const morgan = require('morgan')
 const path = require('path');
 const configViewEngine = require('./config/viewEngine');
 const { log } = require('console');
-const webRoutes = require('./routes/web')
+const webRoutes = require('./routes/web');
+const connection = require('./config/db');
 
-// conection sql
-const mysql = require('mysql2');
-
-const connection  = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'test',
-});
-
+console.log(process.env);
 connection.query('SELECT * FROM users', function(error, results, fields) {
-  console.log("result" + results);
-  console.log("result" +fields);
 	if(error)
 		throw error;
 		results.forEach(result => {
